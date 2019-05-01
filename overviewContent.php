4bc5f97f24a3528452cc997415ab2d789b0f9e62
Peloso <cspeloso@labs.neit.edu>
2019-04-30 20:57:57 -0400

    github link added to main page

--- overviewContent.php
+++ overviewContent.php
@@ -11,7 +11,8 @@
 
     <h2 style="text-align:center;font-weight:bold;">HOME</h2>
 
-
+    <h3 style="text-align:center;font-weight:bold;">Github Repo Link</h3>
+    <center><a href="https://github.com/cspeloso/SE417ss/blob/master/index.php" id="homeLinkThing">Github Link</a></center>
     
 </body>
 </html>
